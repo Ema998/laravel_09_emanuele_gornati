79def4a5406dcbf97957fffe43388ee83e442e8e
Ema998 <emanuele.gornati3@gmail.com>
2025-05-25 01:22:21 +0200

    commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gameland</title>
+     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
